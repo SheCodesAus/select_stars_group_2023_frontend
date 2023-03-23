f8b056d9002d490f1cb263389e367d2e1ff3f150
jenny-jyl <114784070+jenny-jyl@users.noreply.github.com>
2023-03-23 19:00:27 +1100

    Homepage added

--- selectstars/src/App.jsx
+++ selectstars/src/App.jsx
@@ -8,6 +8,7 @@ import LoginForm from './components/LoginPage/loginform';
 import CreateUserForm from './components/CreateUserForm/CreateUserForm';
 import CreateMentorForm from './components/CreateMentorForm/createMentorForm';
 import Nav from './components/Nav/Nav';
+import HomePage from './components/HomePage/HomePage';
 
 const HeaderLayout = () => {
   return (
@@ -22,10 +23,10 @@ const router = createBrowserRouter([
   {
     element: <HeaderLayout />,
     children: [
-      // {
-      //   path: '/',
-      //   element: <HomePage />,
-      // },
+      {
+        path: '/',
+        element: <HomePage />,
+      },
       {
         path: '/login',
         element: <LoginForm />,
