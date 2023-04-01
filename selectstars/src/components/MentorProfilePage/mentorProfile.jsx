a6c9bf8fb9437905d7a04a59b19b52dd7df5c509
Soda De <soda.demoncerf@gmail.com>
2023-04-02 03:58:04 +1000

    reacommittomain

--- selectstars/src/components/MentorProfilePage/mentorProfile.jsx
+++ selectstars/src/components/MentorProfilePage/mentorProfile.jsx
@@ -1,65 +1,30 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-// import { mentors } from "../../../dummydata";
+
 import MentorCard from "../MentorCard/mentorCard";
 import './mentorProfile.css';
 
 
-// remember to insert [] for usestate and uncomment the use effect once API up (Jen did this)
-
-
-
-function MentorProfile(props) {
-
+function MentorProfile() {
+  // const [mentorData, setMentorData] = useState(mentors);
   const [mentorData, setMentorData] = useState([]);
 
 
-
-  // useEffect(() => {
-  //   fetch(`${import.meta.env.VITE_API_URL}api-token-auth/`,)
-  //   .then((results) => {
-  //     return results.json();
-  //   }).then((data) => {
-  //     setMentorData(data);
-  //   });
-  // }, []);
-
-  // fetch(`${import.meta.env.VITE_API_URL}mentor/`)
-  //     .then((results) => {
-  //       return results.json();
-  //     }).then((data) => {
-  //       setMentorData(data);
-  //     });
-
   useEffect(() => {
-
-
-    // fetch(`${import.meta.env.VITE_API_URL}mentors`)
-    //   .then((results) => {
-    //     return results.json();
-    //   }).then((data) => {
-    //     setMentorData(data);
-    //   });
- 
-
     fetch(`${import.meta.env.VITE_API_URL}mentor/`)
-    .then((results) => {
-      
-      return results.json();
-    }).then((data) => {
-      setMentorData(data);
-    });
-
-    
+      .then((results) => {
+        return results.json();
+      }).then((data) => {
+        setMentorData(data);
+      });
   }, []);
-
 
 
   return (
     <div className="mentorProfilePage">
       <div className="mentorProfilePageBackground"></div>
-      {mentorsData.map((mentor, key) => {
-        return <MentorCard key={key} mentorData={mentor} />
+      {mentorData.map((Mentor, key) => {
+        return <MentorCard key={key} mentorData={Mentor} />
       })}
 
     </div>
