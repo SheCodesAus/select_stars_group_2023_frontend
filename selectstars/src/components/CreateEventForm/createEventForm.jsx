a6f188ce8971ff8f667634ca53d0585b5bb03ae0
Sarah-Hashmi-Codes <114784014+Sarah-Hashmi-Codes@users.noreply.github.com>
2023-04-04 19:23:26 +1000

    Fixed event form
    Mentor field not required.

--- selectstars/src/components/CreateEventForm/createEventForm.jsx
+++ selectstars/src/components/CreateEventForm/createEventForm.jsx
@@ -1,19 +1,41 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import './createEventForm.css';
 
 function CreateEventForm() {
     const [eventDetails, setEventDetails] = useState({
-        eventName: '',
-        eventDate: '',
-        eventLocation: '',
-        eventType:'',
-        description:''
+        title: '',
+        start_date: '',
+        end_date:'',
+        location: 'Sydney',
+        event_type:'Flash',
+        description:'',
+       
 
     });
 
+    // const [mentors,setmentors] = useState([])
+
+    
+    // useEffect(() =>{
+    //     const token = window.localStorage.getItem('token')
+    //     fetch(`${import.meta.env.VITE_API_URL}mentor/`, {
+    //         method: 'get',
+    //         headers: {                                 
+    //             'Content-Type': "application/json",  
+    //             'Authorization':`token ${token}`          
+    //         },
+    //     }).then((data)=>{          // data = return of fetch
+    //         return data.json()   // convert data to json
+    //     }).then((mentors) => {      //mentors = data.json
+    //         setmentors(mentors)     // setting state of mentors to all mentors
+    //     })
+    // },[])
+
+
     const navigate = useNavigate();
 
+    
     const handleChange = (event) => {
         const { id, value } = event.target;
         setEventDetails((prevEventDetails) => ({
@@ -22,18 +44,26 @@ function CreateEventForm() {
         }));
     };
 
+    // const handleMentorChange = (event) => {
+    //     console.log(parseFloat(event.target.value))
+    //     setEventDetails((prevEventDetails) => ({
+    //         ...prevEventDetails,
+    //         [event.target.id]: [parseFloat(event.target.value)]
+    //     }));
+    // };
+
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log('Entering event details', eventDetails);
 
-    //    {postData().then((response)=>{
-    //         if (response.detail == 'invalid token.'){
-    //             alert ('Please login to create event')
-    //             navigate('/login')
-    //         }
-    //         else {navigate('/event/${response.id}')};
-    //     })
-    // };
+       {postData().then((response)=>{
+            if (response.detail == 'invalid token.'){
+                alert ('Please login to create event')
+                navigate('/login')
+            }
+            else {navigate("/")};
+        })
+    };
      };
 
 
@@ -59,12 +89,12 @@ function CreateEventForm() {
             <form>
                 <div className="eventField">
                     <label htmlFor='image'>Image:</label>
-                    <input type="url" placeholder="Link of image" id="event_image" onChange={handleChange} />
+                    <input type="url" placeholder="Link of image" id="image" onChange={handleChange} />
                 </div>
 
                 <div className="eventField">
                     <label htmlFor='event_name'>Event Name:</label>
-                    <input type="text" placeholder="Enter name of event" id="event_name" onChange={handleChange} />
+                    <input type="text" placeholder="Enter name of event" id="title" onChange={handleChange} />
                 </div>
 
                 {/* <div className="eventField">
@@ -75,18 +105,18 @@ function CreateEventForm() {
                 <div className="eventField">
                     <label htmlFor='location'>Location:</label>
                     <select id='location' onChange={handleChange}> 
-                    <option value="sydney">Sydney</option>
-                    <option value="brisbane">Brisbane</option>
-                    <option value="perth">Perth</option>
+                    <option value="Sydney">Sydney</option>
+                    <option value="Brisbane">Brisbane</option>
+                    <option value="Perth">Perth</option>
                 </select>
                 </div>
 
                 <div className="eventField">
                     <label htmlFor='type'>Event Type:</label>
-                    <select id='type' onChange={handleChange}>
-                    <option value="flash">Flash</option>
-                    <option value="plus">Plus</option>
-                    <option value="one day workshop">One Day Workshop</option>
+                    <select id='event_type' onChange={handleChange}>
+                    <option value="Flash">Flash</option>
+                    <option value="Plus">Plus</option>
+                    <option value="One Day Workshop">One Day Workshop</option>
                 </select>
                 </div>
 
@@ -94,11 +124,16 @@ function CreateEventForm() {
                     <label htmlFor='description'>Description:</label>
                     <input type="text" placeholder="Enter description" id="description" onChange={handleChange} />
                 </div>
+                
+                {/* <div className="eventField">
+                    <label htmlFor='mentors'>Mentors Assigned:</label>
+                    <select id='mentors' onChange={handleMentorChange}>
+                        {mentors.map((mentor) => {
+                            return <option key={mentor.id} value={mentor.id}> {mentor.first_name} {mentor.last_name}</option>
+                        })}
+                    </select>
+                </div> */}
 
-                <div className="eventField">
-                    <label htmlFor='assigned_mentor'>Mentors Assigned:</label>
-                    <input type="text" placeholder="Assign a mentor" id="assigned_mentor" onChange={handleChange} />
-                </div>
 
                 <div className="eventField">
                     <label htmlFor='start_date'>Start Date:</label>
--- selectstars/src/components/EventCard/EventCard.jsx
+++ selectstars/src/components/EventCard/EventCard.jsx
@@ -8,17 +8,18 @@ function EventCard(props) {
   return (
     <div className="event-card">
       <img src={eventData.image}></img>
+      <h2>{eventData.title}</h2>
       <h2>{eventData.event_type}</h2>
-      <h4>City: {eventData.location}</h4>
-      <h4>Description: {eventData.description}</h4>
-      <h4>Start time: {eventData.start_date}</h4>
-      <h4>End time: {eventData.end_date}</h4>
-      <h4>Mentors: {eventData.mentors}</h4>
+      <h4>{eventData.location}</h4>
+      <h4>{eventData.description}</h4>
+      <h4>Start Date{eventData.start_date}</h4>
+      <h4>End Date: {eventData.end_date}</h4>
+      {/* <h4>Mentors: {eventData.mentors}</h4>
       <ul>
         {eventData.mentors.map((mentorsData, key) => {
           return <li key={key}>{mentorsData.amount}</li>;
         })}
-      </ul>
+      </ul> */}
     </div>
 
   );
