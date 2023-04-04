d4a389cef326afa745c818b3b7e9cb08efbe3204
jenny-jyl <114784070+jenny-jyl@users.noreply.github.com>
2023-04-04 18:18:36 +1000

    casing changes

---
+++ selectstars/src/components/AllMentorsList/AllMentors.jsx
@@ -0,0 +1,60 @@
+import React, {useState, useEffect} from "react";
+// import { useParams } from "react-router-dom";
+import MentorCard from "../MentorCard";
+import './allMentors.css';
+
+
+
+  
+function MentorProfile() {
+  // const { id } = useParams();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [search, setSearch] = useState("")
+  const [mentorsData, setMentorsData] = useState([]);
+
+  useEffect(() => {
+    const isLoggedIn = true;
+    if (!isLoggedIn) {
+        history.push('/login');
+    } else {
+        setIsLoggedIn(true);
+        fetch(`${import.meta.env.VITE_API_URL}mentor`)
+            .then((results) => {
+                return results.json();
+            })
+            .then((data) => {
+                setMentorsData(data)
+            })
+    }
+}, []);
+// useEffect(() => {
+//   if (search == "") {
+//       setFilterData(mentorsData)
+//   } else if (search.length > 0) {
+//       const data = mentorsData.filter((event) => mentor.mentor_type.toLowerCase().includes(search.toLowerCase()))
+//       setFilterData(data)
+//   }
+// }, [search, mentorsData]);
+
+// const handleChange = (mentor) => {
+//   setSearch(mentor.target.value)
+// }
+
+  return isLoggedIn ? (
+    <div className="allMentors-page">
+  
+        <div className="background-image"></div>
+  
+        <div className="allMentorsPage-title">
+          <h1>All Mentors</h1>
+          <div classNmae="allMentorSection">
+            {mentorsData.map((mentorsData, key) => {
+                    return <MentorCard key={key} mentorsData={mentorsData} />
+                })}
+          </div>
+        </div>
+        </div>
+      ) : null;
+};
+
+export default MentorProfile;
