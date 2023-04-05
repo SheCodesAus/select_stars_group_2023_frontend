17d1147cf8d05797677c77820a06ad2eef80021f
Soda De <soda.demoncerf@gmail.com>
2023-04-05 16:43:04 +1000

    Put in a search function

--- selectstars/src/components/AllMentorsList/allMentors.jsx
+++ selectstars/src/components/AllMentorsList/allMentors.jsx
@@ -1,75 +1,77 @@
-import React, {useState, useEffect} from "react";
-// import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import MentorCard from "../MentorCard";
-import './allMentors.css';
+import "./allMentors.css";
 
-
-
-  
 function MentorProfile() {
-  // const { id } = useParams();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [search, setSearch] = useState("")
+  const [search, setSearch] = useState("");
   const [mentorsData, setMentorsData] = useState([]);
   const [techStack, setTechStack] = useState([]);
-
+  const [filteredMentorsData, setFilteredMentorsData] = useState([]);
 
   useEffect(() => {
     const isLoggedIn = true;
     if (!isLoggedIn) {
-        history.push('/login');
+      history.push("/login");
     } else {
-        setIsLoggedIn(true);
-        fetch(`${import.meta.env.VITE_API_URL}mentor`)
-            .then((results) => {
-                return results.json();
-            })
-            .then((data) => {
-                setMentorsData(data)
-            })
+      setIsLoggedIn(true);
+      fetch(`${import.meta.env.VITE_API_URL}mentor`)
+        .then((results) => {
+          return results.json();
+        })
+        .then((data) => {
+          setMentorsData(data);
+          setFilteredMentorsData(data);
+        });
     }
   }, []);
 
-
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}tech_stack/`)
-    .then((results) => {
+      .then((results) => {
         return results.json();
-    })
-    .then((data) => {
+      })
+      .then((data) => {
         setTechStack(data);
-    })
+      });
   }, []);
-// useEffect(() => {
-//   if (search == "") {
-//       setFilterData(mentorsData)
-//   } else if (search.length > 0) {
-//       const data = mentorsData.filter((event) => mentor.mentor_type.toLowerCase().includes(search.toLowerCase()))
-//       setFilterData(data)
-//   }
-// }, [search, mentorsData]);
 
-// const handleChange = (mentor) => {
-//   setSearch(mentor.target.value)
-// }
+  useEffect(() => {
+    if (search === "") {
+      setFilteredMentorsData(mentorsData);
+    } else {
+      const filteredData = mentorsData.filter((mentor) =>
+      mentor.mentor_type && mentor.mentor_type.toLowerCase().includes(search.toLowerCase())
+      );
+      setFilteredMentorsData(filteredData);
+    }
+  }, [search, mentorsData]);
+
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
 
   return isLoggedIn ? (
     <div className="allMentors-page">
-  
-        <div className="background-image"></div>
-  
-        <div className="allMentorsPage-title">
-          <h1>All Mentors</h1>
-          </div>
-
-          <div className="allMentorSection">
-            {mentorsData.map((mentorsData, key) => {
-                    return <MentorCard key={key} mentorsData={mentorsData} techStack={techStack}/>
-                })}
-          </div>
-
-        </div>
-      ) : null;
-};
+      <div className="background-image"></div>
+      <div className="allMentorsPage-title">
+        <h1>All Mentors</h1>
+      </div>
+      <input
+        type="search"
+        value={search}
+        onChange={handleSearch}
+        placeholder="Search by mentor name"
+        className="searchFilter"
+      />
+      <div className="allMentorSection">
+        {filteredMentorsData.map((mentorData, key) => {
+          return <MentorCard key={key} mentorsData={mentorData} techStack={techStack} />;
+        })}
+      </div>
+    </div>
+  ) : null;
+}
 
 export default MentorProfile;
